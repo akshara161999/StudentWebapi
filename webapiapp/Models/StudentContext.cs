6a82a1f1030905a59fe56f1394aa1dbeeb284a52
anil <anil.net9948@gmail.com>
2023-08-31 11:34:19 -0400

    Committing the student Web Api application created

---
+++ webapiapp/Models/StudentContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace webapiapp.Models;
+
+public partial class StudentContext : DbContext
+{
+    public StudentContext()
+    {
+    }
+
+    public StudentContext(DbContextOptions<StudentContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<StudentDetail> StudentDetails { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=localhost\\MSSQLSERVER01;Database=Student;Encrypt=True;TrustServerCertificate=True;Trusted_Connection=True;Integrated Security=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<StudentDetail>(entity =>
+        {
+            entity.ToTable("StudentDetail");
+
+            entity.Property(e => e.Country)
+                .HasMaxLength(25)
+                .IsFixedLength();
+            entity.Property(e => e.Name)
+                .HasMaxLength(10)
+                .IsFixedLength();
+            entity.Property(e => e.RollNumber)
+                .HasMaxLength(10)
+                .IsFixedLength();
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
